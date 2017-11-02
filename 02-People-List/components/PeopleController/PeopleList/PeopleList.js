54755fd998e6c3928fa58a929410b0c6013d8e14
Semih Gökduman <semihgokduman@gmail.com>
2017-11-02 11:39:46 +0100

    part 2

---
+++ 02-People-List/components/PeopleController/PeopleList/PeopleList.js
@@ -0,0 +1,58 @@
+(function () {
+  const currentDocument = document.currentScript.ownerDocument;
+
+  function _createPersonListElement(self, person) {
+  let li = currentDocument.createElement('LI');
+  li.innerHTML = person.name;
+  li.className = 'people-list__name'
+  li.onclick = () => {
+    let event = new CustomEvent("PersonClicked", {
+      detail: {
+        personId: person.id
+      },
+      bubbles: true
+    });
+    self.dispatchEvent(event);
+  }
+  return li;
+}
+
+
+  class PeopleList extends HTMLElement {
+    constructor() {
+      // If you define a constructor, always call super() first as it is required by the CE spec.
+      super();
+    }
+
+    connectedCallback() {
+      // Create a Shadow DOM using our template
+      const shadowRoot = this.attachShadow({ mode: 'open' });
+      const template = currentDocument.querySelector('#people-list-template');
+      const instance = template.content.cloneNode(true);
+      shadowRoot.appendChild(instance);
+    }
+    
+    get list() {
+      return this._list;
+    }
+
+    set list(list) {
+      this._list = list;
+      this.render();
+    }
+
+    render() {
+  let ulElement = this.shadowRoot.querySelector('.people-list__list');
+  ulElement.innerHTML = '';
+
+  this.list.forEach(person => {
+    let li = _createPersonListElement(this, person);
+    ulElement.appendChild(li);
+  });
+}
+
+  }
+
+  customElements.define('people-list', PeopleList);
+})();
+
